933be990e005a68fe99c6dd63b5117c477f8f55b
w43312im <insiya.mullamitha@student.manchester.ac.uk>
2024-02-04 16:16:40 +0000

    Unsplash API for city images

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Overview 🌐
 
-A simple React-based weather application that allows users to check the current weather conditions for a specified city. The app uses the OpenWeatherMap API to fetch real-time weather data.
+A simple React-based weather application that allows users to check the current weather conditions for a specified city. The app uses the OpenWeatherMap API to fetch real-time weather data and the Unsplash API to fetch city images.
 
 ## Getting Started 🚀
 
@@ -13,12 +13,15 @@ git clone https://github.com/insiyamullamitha/weather-app.git
 ```
 
 2. Install dependencies:
-   
+
 ```
 npm install
 ```
 
-3. Create a **'.env'** file in the project root and set your OpenWeatherMap API key as **'REACT_APP_API_KEY'**
+3. Create a **'.env'** file in the project root
+
+   - Set your OpenWeatherMap API key as **'REACT_APP_WEATHER_API_KEY'**
+   - Set your Unsplash API access key as **'REACT_APP_UNSPLASH_API_KEY'**
 
 4. Run the app using **'npm start'**
 
@@ -26,15 +29,15 @@ npm install
 
 ## Features ✨
 
-* **City Search:** Enter the name of the city to get the latest weather information.
-* **Current Date Display:** Shows the current date in a user-friendly format.
-* **Temperature Information:** Displays the temperature in Celsius along with a brief weather description.
-* **Error Handling:** Alerts the user if the entered city is not found.
+- **City Search:** Enter the name of the city to get the latest weather information.
+- **Current Date Display:** Shows the current date in a user-friendly format.
+- **Temperature Information:** Displays the temperature in Celsius along with a brief weather description.
+- **City Image:** Displays appropriate image of entered city alongside weather information.
+- **Error Handling:** Alerts the user if the entered city is not found.
 
 ## Technologies Used 🛠️
 
-* React
-* Axios
-* OpenWeatherMap API
-
-
+- React
+- Axios
+- OpenWeatherMap API
+- Unsplash API
--- src/Weather.js
+++ src/Weather.js
@@ -4,12 +4,24 @@ import axios from "axios";
 const Weather = () => {
   const [city, setCity] = useState("Manchester");
   const [weather, setWeather] = useState(null);
-  const [currentDate, setCurrentDate] = useState("Sunday 4 February 2024");
+  const [currentDate, setCurrentDate] = useState("--");
+  const [cityImage, setCityImage] = useState(null);
+
+  const fetchCityImage = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.unsplash.com/search/photos?query=${city}&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`
+      );
+      setCityImage(response.data.results[0].urls.regular);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const fetchWeather = async () => {
     try {
       const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
       );
       setWeather(response.data);
     } catch (error) {
@@ -32,38 +44,51 @@ const Weather = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     fetchWeather();
+    fetchCityImage();
     getDate();
   };
   return (
-    <div className="weather-container">
-      <div className="weather-form">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={city}
-            onChange={(event) => setCity(event.target.value)}
-            placeholder="Enter city"
-            className="weather-input"
-          />
-        </form>
-        <div className="weather-info">
-          <p className="location">
-            {(weather && `${weather.name}, ${weather.sys.country}`) ||
-              "Manchester, GB"}
-          </p>
-          <p id="date" className="date">
-            {currentDate}
-          </p>
-          <p className="temperature-info">
-            {(weather && `${Math.round(weather.main.temp - 273.15)}°C`) ||
-              "5°C"}
-          </p>
-          <p className="weather-description">
-            {(weather && weather.weather[0].description) || "Rainy"}
-          </p>
+    <>
+      {weather && (
+        <div
+          className="left-section"
+          style={{
+            backgroundImage: cityImage
+              ? `url(${cityImage})`
+              : `url(${process.env.PUBLIC_URL}/images/manchester.jpg)`,
+          }}
+        ></div>
+      )}
+      <div className="right-section">
+        <div className="weather-form">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={city}
+              onChange={(event) => setCity(event.target.value)}
+              placeholder="Enter city"
+              className="weather-input"
+            />
+          </form>
+          <div className="weather-info">
+            <p className="location">
+              {(weather && `${weather.name}, ${weather.sys.country}`) ||
+                "--, --"}
+            </p>
+            <p id="date" className="date">
+              {currentDate}
+            </p>
+            <p className="temperature-info">
+              {(weather && `${Math.round(weather.main.temp - 273.15)}°C`) ||
+                "--°C"}
+            </p>
+            <p className="weather-description">
+              {(weather && weather.weather[0].description) || "--"}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
